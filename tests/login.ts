385bf60ee003a66b5f16eabbc2a529eecbc6234d
thananJayan <thananjayan1988@gmail.com>
2024-08-14 13:42:14 +0000

    UI Changes

--- tests/login.ts
+++ tests/login.ts
@@ -4,7 +4,7 @@ import { test as base, expect } from '@playwright/test'
 const test = base.extend({
     webPage: async({page}, use) =>{
         //set up
-        await page.goto('https://www.saucedemo.com/');
+        await page.goto('https://www.saucedemo.com');
         //use fixture
         use(page)
 
