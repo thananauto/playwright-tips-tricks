d72ff11d285c4fac96096c07bfb6da52999c1a8b
thananJayan <thananjayan1988@gmail.com>
2024-08-15 16:34:18 +0000

    Only changes

---
+++ tests/title.spec.ts
@@ -0,0 +1,42 @@
+import { test, expect } from '@playwright/test'
+
+ 
+test.describe.configure({mode: 'serial'})
+
+test.describe('Check test flow',{ tag: '@pw-test'}, async () => {
+    type objperson ={
+        id: string,
+        name: string,
+        data : {}
+    }
+
+   
+   
+    test('first test', { tag: '@google'}, async ( { page }, testInfo) => {
+        
+       // await page.go(')
+       await page.goto('https://www.google.com')
+        await expect(page).toHaveURL(/google/)
+        
+        console.log(testInfo.title)
+        
+    })
+    test('second test', async ( { page, browserName}, testInfo) => {
+        //skip the test case
+        test.fixme(browserName === 'chromium', 'Test need to be fixed')
+        await page.goto('https://www.amazon.com')
+        await expect( page).toHaveURL(/amazon/)
+        console.log(testInfo.title)
+        
+    })
+
+    test('third test', async ( {page}, testInfo) => {
+        await page.goto('https://www.ebay.com')
+        await expect( page).toHaveURL(/ebay/)
+        console.log(test.info().title)
+        
+    })
+
+   
+    
+})
